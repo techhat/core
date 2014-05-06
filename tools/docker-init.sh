08bc566f388a3960741269a7a9124d97cc8fdbed
Victor Lowther <victor.lowther@gmail.com>
2014-05-05 21:30:43 -0500

    Speed up docker-init.sh by not crossing filesystem boundaries.

--- tools/docker-init.sh
+++ tools/docker-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
 if grep -q crowbar /etc/passwd; then
-    find /var /home  -user crowbar -exec chown "$OUTER_UID" '{}' ';'
+    find /var /home  -xdev -user crowbar -exec chown "$OUTER_UID" '{}' ';'
     usermod -o -u "$OUTER_UID" crowbar
 else
     useradd -o -U -u "$OUTER_UID" \
@@ -10,14 +10,17 @@ else
         crowbar
 fi
 if grep -q crowbar /etc/group; then
-    find /var /home -group crowbar -exec chown "$OUTER_UID:$OUTER_GID" '{}' ';'
+    find /var /home -xdev -group crowbar -exec chown "$OUTER_UID:$OUTER_GID" '{}' ';'
     groupmod -o -g "$OUTER_GID" crowbar
     usermod -g "$OUTER_GID" crowbar
 fi
 mkdir -p /root/.ssh
 printf "%s\n" "$SSH_PUBKEY" >> /root/.ssh/authorized_keys
 
-[[ $1 ]] && "$@"
+if [[ $1 ]]; then
+    "$@"
+fi
 . /etc/profile
 export PATH=$PATH:/opt/opencrowbar/core/bin
+/bin/bash -i
 rm -rf /tftpboot/nodes
