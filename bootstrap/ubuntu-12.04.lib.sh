83f6d83253b316762b1bff63420d305760a7971e
galthaus <galthaus@austin.rr.com>
2014-10-27 00:25:46 -0500

    Add missing install command.

--- bootstrap/ubuntu-12.04.lib.sh
+++ bootstrap/ubuntu-12.04.lib.sh
@@ -4,5 +4,5 @@ update-locale LANG=en_US.UTF-8
 install_prereqs() {
     apt-get -y --force-yes update
     apt-get -y --force-yes dist-upgrade
-    apt-get -y --force-yes build-essential curl
+    apt-get -y --force-yes install build-essential curl
 }
