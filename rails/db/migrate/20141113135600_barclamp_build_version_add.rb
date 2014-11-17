72e6118083f969b4e257710ff8e693a8ee7cee49
galthaus <galthaus@austin.rr.com>
2014-11-17 10:27:43 -0600

    Update copyright

--- rails/db/migrate/20141113135600_barclamp_build_version_add.rb
+++ rails/db/migrate/20141113135600_barclamp_build_version_add.rb
@@ -1,4 +1,4 @@
-# Copyright 2014, Victor Lowther
+# Copyright 2014, Greg Althaus
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
